a2247e70cea5bfa4d585667eeac0a8441a4a4890
dohoaigiang <tranglinh462005@gmail.com>
2022-11-16 23:54:38 +0700

    DOANDIDONG1

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, FlatList } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      categories: [
+        {id: 1, name: 'sản phẩm skincare'},
+        {id: 2, name: 'sản phẩm chăm sóc tóc'},
+        {id: 3, name: 'nước hoa'},
+        {id: 4, name: 'sản phẩm làm trắng da'},
+        {id: 5, name: 'kem chống nắng'},
+        {id: 5, name: 'mặt nạ'},
+    ]
+    };
+  }
   render(){
+    const {categories} = this.state;
   return (
-    <View style={styles.container}>
-      <CategoryListItem/>
-    </View>
+    <FlatList
+      data = {categories}
+      renderItem = {({item})=><CategoryListItem category={item}/>} 
+      keyExtractor={item=>'${item.id}'}
+      contentContainerStyle={{paddingLeft:16, paddingRight:16}}
+    />
   );
 }
 }
@@ -17,7 +34,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'stretch',
-    backgroundColor: '#fff',
+    backgroundColor:'#fff',
     justifyContent: 'center',
     paddingLeft: 16,
     paddingRight: 16
--- components/CategoryListItem.js
+++ components/CategoryListItem.js
@@ -8,9 +8,10 @@ import {
 import skincareImage from '../assets/skincare.png'
 
 export default function CategoryListItem(props) {
+  const {category} = props;
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>CategoryListItem</Text>
+      <Text style={styles.title}>{category.name}</Text>
       <Image style={styles.categoryImage} source={skincareImage}/>
     </View>
   );
@@ -21,8 +22,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
     borderRadius: 4,
-    backgroundColor: '#FFF',
-    shadowColor: '#000',
+    backgroundColor: '#f0f8ff',
+    shadowColor: '#ffe4c4',
     shadowOpacity: 0.3,
     shadowRadius: 10,
     shadowOffset: {width:0, height:0},
